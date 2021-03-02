994f0789ee489ffe49079cb2e0f9ce9d621aaee4
Tim Coulson <tim.r.coulson@gmail.com>
2021-03-02 12:11:01 +0000

    feat: use rpc names, not returns types for uniqueness|

--- example/authors/authors_grpc.pb.go
+++ example/authors/authors_grpc.pb.go
@@ -12,7 +12,6 @@ import (
 
 // This is a compile-time assertion to ensure that this generated file
 // is compatible with the grpc package it is being compiled against.
-// Requires gRPC-Go v1.32.0 or later.
 const _ = grpc.SupportPackageIsVersion7
 
 // AuthorsClient is the client API for Authors service.
@@ -78,7 +77,7 @@ type UnsafeAuthorsServer interface {
 }
 
 func RegisterAuthorsServer(s grpc.ServiceRegistrar, srv AuthorsServer) {
-	s.RegisterService(&Authors_ServiceDesc, srv)
+	s.RegisterService(&_Authors_serviceDesc, srv)
 }
 
 func _Authors_GetAuthors_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
@@ -117,10 +116,7 @@ func _Authors_LoadAuthors_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
-// Authors_ServiceDesc is the grpc.ServiceDesc for Authors service.
-// It's only intended for direct use with grpc.RegisterService,
-// and not to be introspected or modified (even as a copy)
-var Authors_ServiceDesc = grpc.ServiceDesc{
+var _Authors_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "authors.Authors",
 	HandlerType: (*AuthorsServer)(nil),
 	Methods: []grpc.MethodDesc{
