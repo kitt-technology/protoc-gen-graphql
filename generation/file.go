f01cbb37ceebd80813c273dd5b0f9d21195286da
Harry Payne <harryfpayne@gmail.com>
2021-12-18 13:48:24 +0000

    chore(util): Move package parsing to util

--- generation/file.go
+++ generation/file.go
@@ -6,15 +6,13 @@ import (
 	"github.com/kitt-technology/protoc-gen-graphql/generation/imports"
 	"github.com/kitt-technology/protoc-gen-graphql/generation/types"
 	"github.com/kitt-technology/protoc-gen-graphql/generation/types/enum"
+	"github.com/kitt-technology/protoc-gen-graphql/generation/util"
 	"github.com/kitt-technology/protoc-gen-graphql/graphql"
 	"google.golang.org/protobuf/compiler/protogen"
 	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/types/descriptorpb"
 	"sort"
 	"strings"
 	"text/template"
-	"unicode"
-	"unicode/utf8"
 )
 
 const fileTpl = `
@@ -67,7 +65,7 @@ func New(file *protogen.File) (f File) {
 	}
 
 	if proto.HasExtension(file.Proto.Options, graphql.E_Package) {
-		importPath, gqlPkg, ok := parseGraphqlPackage(file.Proto)
+		importPath, gqlPkg, ok := util.ParseGraphqlPackage(file.Proto)
 		if !ok {
 			panic("invalid graphql.package: " + file.Proto.GetName())
 		}
@@ -133,75 +131,4 @@ func (f File) ToString() string {
 		out += msg.Generate()
 	}
 	return out
-}
-
-// The parsing for graphql.package is copied from protoc-gen-go's package parsing
-// https://github.com/golang/protobuf/blob/ae97035608a719c7a1c1c41bed0ae0744bdb0c6f/protoc-gen-go/generator/generator.go#L275
-
-func parseGraphqlPackage(file *descriptorpb.FileDescriptorProto) (importPath string, pkg string, ok bool) {
-	if !proto.HasExtension(file.Options, graphql.E_Package) {
-		return "", "", false
-	}
-	graphqlPackage := proto.GetExtension(file.Options, graphql.E_Package).(string)
-
-	sc := strings.Index(graphqlPackage, ";")
-	if sc >= 0 {
-		return graphqlPackage[:sc], cleanPackageName(graphqlPackage[sc+1:]), true
-	}
-	slash := strings.LastIndex(graphqlPackage, "/")
-	if slash >= 0 {
-		return graphqlPackage, cleanPackageName(graphqlPackage[slash+1:]), true
-	}
-	return "", cleanPackageName(graphqlPackage), true
-}
-
-func cleanPackageName(name string) string {
-	name = strings.Map(badToUnderscore, name)
-	// Identifier must not be keyword or predeclared identifier: insert _.
-	if isGoKeyword[name] {
-		name = "_" + name
-	}
-	// Identifier must not begin with digit: insert _.
-	if r, _ := utf8.DecodeRuneInString(name); unicode.IsDigit(r) {
-		name = "_" + name
-	}
-	return name
-}
-
-var isGoKeyword = map[string]bool{
-	"break":       true,
-	"case":        true,
-	"chan":        true,
-	"const":       true,
-	"continue":    true,
-	"default":     true,
-	"else":        true,
-	"defer":       true,
-	"fallthrough": true,
-	"for":         true,
-	"func":        true,
-	"go":          true,
-	"goto":        true,
-	"if":          true,
-	"import":      true,
-	"interface":   true,
-	"map":         true,
-	"package":     true,
-	"range":       true,
-	"return":      true,
-	"select":      true,
-	"struct":      true,
-	"switch":      true,
-	"type":        true,
-	"var":         true,
-}
-
-// badToUnderscore is the mapping function used to generate Go names from package names,
-// which can be dotted in the input .proto file.  It replaces non-identifier characters such as
-// dot or dash with underscore.
-func badToUnderscore(r rune) rune {
-	if unicode.IsLetter(r) || unicode.IsDigit(r) || r == '_' {
-		return r
-	}
-	return '_'
 }
