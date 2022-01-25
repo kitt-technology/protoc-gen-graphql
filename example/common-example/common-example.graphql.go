f74e17106262cb2b048c141c94089280579545e2
Harry Payne <harryfpayne@gmail.com>
2022-01-25 11:37:41 +0000

    fix: common protos importing self

--- example/common-example/common-example.graphql.go
+++ example/common-example/common-example.graphql.go
@@ -80,3 +80,68 @@ func (msg *Int32Range) XXX_GraphqlArgs() gql.FieldConfigArgument {
 func (msg *Int32Range) XXX_Package() string {
 	return "common_example"
 }
+
+var MoneyRangeGraphqlType = gql.NewObject(gql.ObjectConfig{
+	Name: "MoneyRange",
+	Fields: gql.Fields{
+		"min": &gql.Field{
+			Type: MoneyGraphqlType,
+		},
+		"max": &gql.Field{
+			Type: MoneyGraphqlType,
+		},
+	},
+})
+
+var MoneyRangeGraphqlInputType = gql.NewInputObject(gql.InputObjectConfig{
+	Name: "MoneyRangeInput",
+	Fields: gql.InputObjectConfigFieldMap{
+		"min": &gql.InputObjectFieldConfig{
+			Type: MoneyGraphqlInputType,
+		},
+		"max": &gql.InputObjectFieldConfig{
+			Type: MoneyGraphqlInputType,
+		},
+	},
+})
+
+var MoneyRangeGraphqlArgs = gql.FieldConfigArgument{
+	"min": &gql.ArgumentConfig{
+		Type: MoneyGraphqlInputType,
+	},
+	"max": &gql.ArgumentConfig{
+		Type: MoneyGraphqlInputType,
+	},
+}
+
+func MoneyRangeFromArgs(args map[string]interface{}) *MoneyRange {
+	return MoneyRangeInstanceFromArgs(&MoneyRange{}, args)
+}
+
+func MoneyRangeInstanceFromArgs(objectFromArgs *MoneyRange, args map[string]interface{}) *MoneyRange {
+	if args["min"] != nil {
+		val := args["min"]
+		objectFromArgs.Min = MoneyFromArgs(val.(map[string]interface{}))
+	}
+	if args["max"] != nil {
+		val := args["max"]
+		objectFromArgs.Max = MoneyFromArgs(val.(map[string]interface{}))
+	}
+	return objectFromArgs
+}
+
+func (objectFromArgs *MoneyRange) FromArgs(args map[string]interface{}) {
+	MoneyRangeInstanceFromArgs(objectFromArgs, args)
+}
+
+func (msg *MoneyRange) XXX_GraphqlType() *gql.Object {
+	return MoneyRangeGraphqlType
+}
+
+func (msg *MoneyRange) XXX_GraphqlArgs() gql.FieldConfigArgument {
+	return MoneyRangeGraphqlArgs
+}
+
+func (msg *MoneyRange) XXX_Package() string {
+	return "common_example"
+}
