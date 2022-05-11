b49d401f95cc02415b2615ce60b6f669e341956e
Harry Payne <harryfpayne@gmail.com>
2022-05-11 17:29:29 +0100

    feat: add to example

--- generation/imports/main.go
+++ generation/imports/main.go
@@ -3,7 +3,6 @@ package imports
 const (
 	WrappersPbImport  = "google.golang.org/protobuf/types/known/wrapperspb"
 	TimestampPbImport = "google.golang.org/protobuf/types/known/timestamppb"
-	FieldMaskPbImport = "google.golang.org/protobuf/types/known/fieldmaskpb"
 	GraphqlAst        = "github.com/graphql-go/graphql/language/ast"
 	PggImport         = "pg \"github.com/kitt-technology/protoc-gen-graphql/graphql\""
 )
