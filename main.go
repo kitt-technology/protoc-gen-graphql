f2d733690f32b6c348c5a67deeeec78c47b37f7c
Kai Konishi-Dukes <30573872+SmashingRhinoceros@users.noreply.github.com>
2024-07-16 13:17:32 +0100

    chore(example): Revert

--- main.go
+++ main.go
@@ -14,8 +14,6 @@ import (
 )
 
 func main() {
-	fmt.Println("os stdin", os.Stdin)
-
 	bytes, _ := ioutil.ReadAll(os.Stdin)
 
 	SupportedFeatures := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
@@ -29,42 +27,34 @@ func main() {
 	opts := protogen.Options{}
 	plugin, _ := opts.New(&req)
 
-	fmt.Println("to gen", req.FileToGenerate)
-	fmt.Println("proto files", req.ProtoFile)
-	for _, file := range plugin.Files {
-		fmt.Println("file gopkg: ", file.GoPackageName)
-		fmt.Println("file name: ", file.GeneratedFilenamePrefix)
-		fmt.Println("file message length: ", len(file.Messages))
-	}
-
 	filesToProcess := make([]*protogen.File, 0)
 	for _, file := range plugin.Files {
 		if shouldProcess(file) {
 			filesToProcess = append(filesToProcess, file)
-			//parsedFile := generation.New(file)
-			//fmt.Println("<<<<", "*1*", parsedFile.Package, "*2*", len(parsedFile.Message), "*3*", file.GeneratedFilenamePrefix, "*4*", len(file.Messages),
-			//	"*5*", len(parsedFile.TypeDefs), "*6*", len(parsedFile.Imports),
-			//	"*7*", len(parsedFile.ImportMap),
-			//	">>>>>")
-			//generateFile := plugin.NewGeneratedFile(file.GeneratedFilenamePrefix+".graphql.go", ".")
-			//_, err = generateFile.Write([]byte(parsedFile.ToString()))
-			//if err != nil {
-			//	panic(err)
-			//}
+			parsedFile := generation.New(file)
+			fmt.Println("<<<<", "*1*", parsedFile.Package, "*2*", len(parsedFile.Message), "*3*", file.GeneratedFilenamePrefix, "*4*", len(file.Messages),
+				"*5*", len(parsedFile.TypeDefs), "*6*", len(parsedFile.Imports),
+				"*7*", len(parsedFile.ImportMap),
+				">>>>>")
+			generateFile := plugin.NewGeneratedFile(file.GeneratedFilenamePrefix+".graphql.go", ".")
+			_, err = generateFile.Write([]byte(parsedFile.ToString()))
+			if err != nil {
+				panic(err)
+			}
 		}
 	}
 
-	parsedFile := generation.NewFromMultiple(filesToProcess)
-	fmt.Println("<<<<", "*1*", parsedFile.Package, "*2*", len(parsedFile.Message),
-		"*5*", len(parsedFile.TypeDefs), "*6*", len(parsedFile.Imports),
-		"*7*", len(parsedFile.ImportMap),
-		">>>>>")
-	generateFile := plugin.NewGeneratedFile(filesToProcess[0].GeneratedFilenamePrefix+".graphql.go", ".")
-	_, err = generateFile.Write([]byte(parsedFile.ToString()))
-	if err != nil {
-		fmt.Println("<<<<ERROR1>>>>")
-		panic(err)
-	}
+	//parsedFile := generation.NewFromMultiple(filesToProcess)
+	//fmt.Println("<<<<", "*1*", parsedFile.Package, "*2*", len(parsedFile.Message),
+	//	"*5*", len(parsedFile.TypeDefs), "*6*", len(parsedFile.Imports),
+	//	"*7*", len(parsedFile.ImportMap),
+	//	">>>>>")
+	//generateFile := plugin.NewGeneratedFile(filesToProcess[0].GeneratedFilenamePrefix+".graphql.go", ".")
+	//_, err = generateFile.Write([]byte(parsedFile.ToString()))
+	//if err != nil {
+	//	fmt.Println("<<<<ERROR1>>>>")
+	//	panic(err)
+	//}
 
 	stdout := plugin.Response()
 	stdout.SupportedFeatures = &SupportedFeatures
